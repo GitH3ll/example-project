c1b96db7657b39014d53cf16d970dbea90a461a3
oleg.dobrovolskiy <oleg.dobrovolskiy@sbermegamarket.ru>
2023-02-16 21:21:02 +0300

    Module_2-1 Add http server

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/GitH3ll/example-project
 
 go 1.20
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"github.com/GitH3ll/example-project/internal/repository"
+	"github.com/GitH3ll/example-project/internal/server"
+	"github.com/GitH3ll/example-project/internal/service"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("ok")
+	userRepo := repository.NewUserRepo("myNewFile.json")
+	controller := service.NewController(userRepo)
+
+	logger := logrus.New()
+
+	srv := server.NewServer(":8000", logger, controller)
+	srv.RegisterRoutes()
+
+	srv.StartServer()
 }
